76060a6b7fac43b4ac860b3df7a58cda617a3a1f
Maria <pascoe_maria@hotmail.com>
2022-04-28 11:53:03 +0100

    Added limit to decrement button

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -3,13 +3,19 @@ import styles from './Counter.module.scss';
 
 
 const Counter = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const limitDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
  
   return (
     <div className={styles.counter}>
       <p>Counter</p>
       <div className={styles.countContainer}>
-        <button className={styles.button} onClick={() => setCount(count - 1)}>-</button>
+        <button className={styles.button} onClick={() =>  limitDecrement()}>-</button>
         <span className={styles.count}>{count}</span>
         <button className={styles.button} onClick={() => setCount(count + 1)}>+</button>
       </div>

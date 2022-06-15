8d30b9976f171c45cff5071981f8f11e31cec6dd
Maria <pascoe_maria@hotmail.com>
2022-06-15 11:06:45 +0100

    Tidied up styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import React, { useState } from 'react';
-import styles from './App.module.scss';
+import React, { useState } from "react";
+import styles from "./App.module.scss";
 //import Counter from './components/Counter';
-import EmployeeCard from './components/Employee';
+import EmployeeCard from "./components/Employee";
 
-import team from './data/team';
-
+import team from "./data/team";
 
 const App = () => {
-
   const getEmployeeCardJsx = (employee) => (
     <div key={employee.id}>
       <EmployeeCard employee={employee} />
     </div>
   );
 
-  return (    
+  return (
     <div className={styles.app}>
       <h1 className={styles.header}>Ticket Tracker</h1>
-      <section className={styles.team}>{team.map(getEmployeeCardJsx)}</section>    
-    </div>    
-   );
-}
+      <section className={styles.team}>{team.map(getEmployeeCardJsx)}</section>
+    </div>
+  );
+};
 
 export default App;
